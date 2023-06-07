6122fbabd9674a10cc3d155cac5a7524c5b5bef0
ThopsD <74445064+thops1925@users.noreply.github.com>
2023-06-08 04:18:53 +0800

    delete

--- app/(server)/api/auth/[...nextauth]/route.ts
+++ app/(server)/api/auth/[...nextauth]/route.ts
@@ -19,7 +19,7 @@ const handler = NextAuth({
 			session.user.id = sessionUser._id.toString();
 			return session;
 		},
-		async signIn({ profile }: { profile: Profile | any }) {
+		async signIn({ profile }: { profile: Profile }) {
 			try {
 				await connectToDB();
 
